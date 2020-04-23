58d06430dd4b8ac47041c696ac5261d905be18fb
MoNaRh-Sys <KostyaMoN@yandex.ru>
2020-04-24 00:22:36 +0500

    added

---
+++ Product of array numbers.pas
@@ -0,0 +1,27 @@
+﻿// Программа для нахождения произведения чисел, введенных пользователем в массив
+LABEL Invalid_input;          // Метка для защиты от неверного ввода
+var
+Numbers: array[1 .. 5] of integer;
+Product_of_array_number, i: integer;
+Begin
+  Product_of_array_number := 1;
+  for i:= 1 to 5 do
+  begin
+    Invalid_input:;
+    write('Введите четное число: ');
+    read(Numbers[i]);
+    if Numbers[i] mod 2 = 0 then
+    begin
+      Product_of_array_number := Product_of_array_number * Numbers[i];
+      continue
+    end
+    else
+    begin
+      writeln('> Неверный ввод (четное число - число, которое делится без остатка на 2)');
+      goto Invalid_input;         // Переход в начало из-за неправильного ввода
+    end
+  end;
+  
+  write('Произведение чисел массива = ', Product_of_array_number);
+
+End.
---
+++ Sum of array numbers.pas
@@ -0,0 +1,25 @@
+﻿LABEL Invalid_input;          // Метка для защиты от неверного ввода
+var
+Numbers: array[1 .. 5] of integer;
+Sum_of_array_number, i: integer;
+Begin
+  for i:= 1 to 5 do
+  begin
+    Invalid_input:;
+    write('Введите четное число: ');
+    read(Numbers[i]);
+    if Numbers[i] mod 2 = 0 then
+    begin
+      Sum_of_array_number := Sum_of_array_number + Numbers[i];
+      continue
+    end
+    else
+    begin
+      writeln('> Неверный ввод (четное число - число, которое делится без остатка на 2)');
+      goto Invalid_input;         // Переход в начало из-за неправильного ввода
+    end
+  end;
+  
+  write('Сумма чисел массива = ', Sum_of_array_number);
+
+End.
