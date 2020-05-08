2cc0634522b8584f6269b3d9f6128ffec9814adf
MoNaRh-Sys <KostyaMoN@yandex.ru>
2020-05-08 09:59:10 +0500

    Story: 08.05.2020

---
+++ Search_max.pas
@@ -0,0 +1,14 @@
+﻿program search_max;
+var
+i, imax: integer;
+number: array[1 .. 10] of integer;
+Begin
+    imax:=1;
+    for i:=1 to 10 do
+    begin
+        read(number[i]);
+        if number[i] > number[imax] then imax := i
+    end;
+    for i:=1 to 10 do write(number[i], ' ');
+    writeln('Наибольшее число a[',imax,']=', number[imax]);
+End.
